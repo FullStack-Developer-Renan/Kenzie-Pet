12d5fc3f48bf9e5c9c3208eb91837c54cba56b55
Renan  Sampaio <renansampaio0401@yahoo.com>
2021-08-02 09:40:45 -0400

    fix(pet_app/models.py): adjusting class Animal e class Group

--- pet_app/models.py
+++ pet_app/models.py
@@ -4,13 +4,14 @@ from django.db.models.fields.related import ForeignKey
 class Group(models.Model):
     name = models.CharField(max_length=255)
     scientific_name = models.CharField(max_length=255)
-    animal = models.ForeignKey("Animal", on_delete=models.CASCADE, related_name="groups")
-
+    
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     age = models.FloatField()
     weight = models.FloatField()
     sex = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="animals")
+
 
 class Characterist(models.Model):
     name = models.CharField(max_length=255)
